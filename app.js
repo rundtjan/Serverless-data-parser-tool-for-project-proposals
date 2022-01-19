a94b6ee07e886b4f2564eae3cfdcb3ffa2cb6d6d
Jan Rundt <jan.rundt@gmail.com>
2022-01-19 16:59:26 +0200

    minor modularization

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+var importHistory = require('./utils/importHistory.js');
+const slackToken = process.env.SLACK_TEST_TOKEN;
+
+app.use(express.static('build'))
+
+//you will need valid channel ids for testing
+app.get('/api/data/:channelid', (req, res) => {
+    importHistory(req.params.channelid, slackToken, res);
+})
+
+module.exports = app
