d6dd88fc14188bcae6e7eda90ef36e8ae28c22c2
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-01-22 11:39:41 +0200

    Added Cors

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,11 @@ const express = require('express')
 const app = express()
 var importHistory = require('./utils/importHistory.js');
 const slackToken = process.env.SLACK_TOKEN;
+const cors = require('cors')
 
-app.use(express.static('build'))
+app.use(cors())
+// app.use(express.static('build'))
+
 
 //you will need valid channel ids for testing
 app.get('/api/data/:channelid', (req, res) => {
