fd5de696816e7fbde168a36e75c5381d0edf9229
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2022-01-24 10:14:14 +0200

    More unit tests

--- backend/tests/unit/getHumanMessagesFromSlack.test.js
+++ backend/tests/unit/getHumanMessagesFromSlack.test.js
@@ -29,3 +29,23 @@ test('list of messages is returned correctly', () => {
     expect(messages).not.toBeNull()
     expect(messages).not.toBeUndefined()
 })
+
+test('returned list is empty if no client_msg_id field', () => {
+    messages = [
+        {   type: 'message',
+            text: 'kissa on suomen suosituin kotieläin',
+            user: 'U02UHPPRMJ6',
+            ts: '1642527720.000700',
+            team: 'T02UNV7V4GZ',
+            blocks: [ [Object] ],
+            reactions: [ [Object] ] },
+        {   type: 'message',
+            text: 'terveppä terve',
+            user: 'U02UHPPRMJ6',
+            ts: '1642527711.000400',
+            team: 'T02UNV7V4GZ',
+            blocks: [ [Object] ] },
+    ]
+    const result = humanMessagesFromSlack(messages)
+    expect(result.length).toBe(0)
+})
