a06761bbbdf2e5669c5a38982ccc9dfe94c41a50
Jan Rundt <jan.rundt@gmail.com>
2022-01-27 14:27:55 +0200

    started work on parametrization

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,24 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
 //you will need valid channel ids for testing
 app.get('/api/data/:channelid', (req, res) => {
   importHistory(req.params.channelid, slackToken, res)
+})
+
+app.post('/api/paramizedData', (req, res) => {
+  const now = Date.now();
+  console.log(now);
+  console.log(req.body)
+  res.send(req.body)
 })
 
 module.exports = app
