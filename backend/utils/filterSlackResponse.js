6a24ce82eedd6ef85f31ac97df51c4ac45fc4464
jt551 <jtwebapps@gmail.com>
2022-01-28 14:55:18 +0200

    fix typos

--- backend/utils/filterSlackResponse.js
+++ backend/utils/filterSlackResponse.js
@@ -21,13 +21,37 @@ const GetThreads = (messages) => {
 
 const GetWordsFromMessages = (messages) => {
   const result = []
+  const temp_word_obj = {}
+  
   messages.forEach((message) => {
     let words = message.text.split(' ')
     words = words.filter(notAnEmoji)
-    words.forEach((e) => result.push(e))
+    words.forEach((word) => {
+      word = RemoveSpecialCharacters(word)
+      word = word.toLowerCase() 
+      if (word in temp_word_obj){
+        temp_word_obj[word]['count'] += 1
+        temp_word_obj[word]['message_ids'].push(message.client_msg_id)
+      } else {
+        temp_word_obj[word] = {
+          'word': word,
+          'message_ids': [message.client_msg_id],
+          'count': 1
+        }
+      }
+          
+    })
   })
+  
+  Object.keys(temp_word_obj).forEach( key => {
+    result.push(temp_word_obj[key])
+  })
+  
+  result.sort((a, b) => b.count - a.count)
   return result
 }
+
+const RemoveSpecialCharacters = (word) => word.replace(/[^\w\såäö£$€]/gi, '')
 
 const GetRealNamesFromSlack = (messages, members) => {
   messages.forEach((elem) => (elem.real_name = members[elem.user]))
