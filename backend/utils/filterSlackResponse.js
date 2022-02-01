c98c97ca213712fb640a75e59cbfd9564de09d6b
jt551 <jtwebapps@gmail.com>
2022-02-01 15:26:16 +0200

    refractor getwords

--- backend/utils/filterSlackResponse.js
+++ backend/utils/filterSlackResponse.js
@@ -35,13 +35,13 @@ const AddThreadToParent = (thread, messages) => {
 const GetWordsFromMessages = (messages) => {
   const result = []
   const temp_word_obj = {}
+
   messages = messages.concat(AddThreadMessages(messages))
   messages.forEach((message) => {
-    let words = message.text.split(' ')
-    words = words.filter(notAnEmoji)
-    words.forEach((word) => {
-      word = RemoveSpecialCharacters(word)
-      word = word.toLowerCase() 
+    const words = message.text.split(' ')
+    const parsedWords = ParseWords(words)
+    
+    parsedWords.forEach((word) => {     
       word in temp_word_obj ? 
         (temp_word_obj[word]['count'] += 1, temp_word_obj[word]['message_ids'].push(message.client_msg_id)) :
         temp_word_obj[word] = Create_Word_Obj(word, message)          
@@ -54,6 +54,16 @@ const GetWordsFromMessages = (messages) => {
   
   result.sort((a, b) => b.count - a.count)
   return result
+}
+
+const ParseWords = (words) => {
+  let parsedWords = words.filter(notAnEmoji)
+  parsedWords = parsedWords.map(word => {
+    word = RemoveSpecialCharacters(word)
+    word = word.toLowerCase()
+    return word
+  })
+  return parsedWords
 }
 
 const AddThreadMessages = (messages) => {
@@ -69,7 +79,8 @@ const Create_Word_Obj = (word, message) => {
   return {
     'word': word,
     'message_ids': [message.client_msg_id],
-    'count': 1
+    'count': 1,
+    'important': false
   } 
 }
 
