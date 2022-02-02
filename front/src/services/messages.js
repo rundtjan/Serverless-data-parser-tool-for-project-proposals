972ac1c587ae511b46a0af2325f0e9ce255bd07a
Jan Rundt <jan.rundt@gmail.com>
2022-02-02 10:42:00 +0200

    url fix

--- front/src/services/messages.js
+++ front/src/services/messages.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
 
 const port = process.env.REACT_APP_BACKEND_PORT || 80 // eslint-disable-line
-const channel = 'general'
-const baseUrl = `http://${window.location.hostname}:${port}/api/data/${channel}`
+const baseUrl = `http://${window.location.hostname}:${port}/api/data/`
 
 const getAll = async(channel) => {
-  const res = await axios.get(`${baseUrl}`)
-  console.log(`${baseUrl}${channel}`)
+  const res = await axios.get(`${baseUrl}${channel}`)
   return res.data
 }
 
