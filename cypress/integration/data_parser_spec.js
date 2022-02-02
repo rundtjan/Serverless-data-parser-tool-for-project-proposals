09904a619eeb5294bcf8ac76e48331aadc1e43de
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2022-02-02 15:04:17 +0200

    first cypress tests

--- cypress/integration/data_parser_spec.js
+++ cypress/integration/data_parser_spec.js
@@ -5,4 +5,11 @@ describe('Data parser tool ', function() {
         cy.contains('Words from messages')
         cy.contains('Make choices')
     })
+
+    it('Message which is a thread has an expander', function() {
+        cy.visit('http://localhost:80')
+        cy.contains('Slack messages')
+        cy.contains('Message massage sent by Jan Rundt')
+        cy.get('.css-9l3uo3').contains('Message massage sent by Jan Rundt')
+    })
 })
