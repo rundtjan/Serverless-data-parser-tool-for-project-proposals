0932ab91aed76898e9d64fe2b54b496946e940a0
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2022-02-03 12:37:29 +0200

    Refactored tests and switched localhost to production env url

--- cypress/integration/data_parser_spec.js
+++ cypress/integration/data_parser_spec.js
@@ -1,13 +1,15 @@
 describe('Data parser tool ', function() {
+    beforeEach(function() {
+        cy.visit('http://135.181.37.120/')
+    })
+
     it('Front page can be opened', function() {
-        cy.visit('http://localhost:80')
         cy.contains('Slack messages')
         cy.contains('Words from messages')
         cy.contains('Make choices')
     })
 
     it('Message which is a thread has an expander', function() {
-        cy.visit('http://localhost:80')
         cy.contains('Slack messages')
         cy.contains('Message massage sent by Jan Rundt')
         cy.get('[id=text-727f403c-f840-4735-abc4-654471c549ed]').contains('Message massage sent by Jan Rundt')
@@ -17,7 +19,6 @@ describe('Data parser tool ', function() {
     })
 
     it('Message which is not a thread does not have an expander', function() {
-        cy.visit('http://localhost:80')
         cy.contains('Slack messages')
         cy.contains('Diba daba sent by Jan Rundt')
         cy.get('[id=text-0b990549-d16b-4ede-a1d5-19fcfa9255ae]').contains('Diba daba sent by Jan Rundt')
