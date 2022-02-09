dbd97bdec7322d19b08dc901af1943e160e3d117
Jan Rundt <jan.rundt@gmail.com>
2022-02-09 14:09:14 +0200

    columns for categories

--- front/src/components/CategoryWords.js
+++ front/src/components/CategoryWords.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+//import { Grid } from '@mui/material'
 import { Typography, Container } from '@mui/material'
 import List from '@mui/material/List'
 
 //import Word from './Word'
 
-const AssignedWords = () => {
-  const assignedWords = useSelector(state => state.assignedWords)
+const CategoryWords = ({ category }) => {
+  const assignedWords = useSelector(state => state.assignedWords.filter(word => word.category === category))
 
   return (
     <Container>
       <Typography variant='h5'>
-        Assigned words
+        { category }
       </Typography>
       <List dense >
         {assignedWords.map(word => (
-          <p key={word.word}>{word.word} {word.category}</p>
+          <p key={word.word}>{word.word} </p>
         ))}
       </List>
     </Container>
   )
 }
 
-export default AssignedWords
+export default CategoryWords
--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -3,14 +3,21 @@ import React from 'react'
 //import Container from '@mui/material/Container'
 import { Grid } from '@mui/material'
 
+import { useSelector } from 'react-redux'
+
 
 import Messages from '../components/Messages'
 import Words from '../components/Words'
 import UserForm from '../components/UserForm'
-import AssignedWords from '../components/AssignedWords'
+import CategoryWords from '../components/CategoryWords'
 
 const HomePage = () => {
+  const categories = useSelector(state => state.categories)
 
+  const addCategories = () => {
+    const categoryElement = categories.map(cat => <Grid key={cat + 'griditem'} item><CategoryWords key={cat + 'cat'} category={cat} /></Grid>)
+    return categoryElement
+  }
 
   return(
     <Grid container direction='row'>
@@ -23,9 +30,7 @@ const HomePage = () => {
       <Grid item>
         <Words />
       </Grid>
-      <Grid item>
-        <AssignedWords />
-      </Grid>
+      { categories.length ? addCategories() : null }
     </Grid>
   )
 }
