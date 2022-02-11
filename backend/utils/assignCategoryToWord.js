e2674556bd4f3ef8e534b28860df1da6e16dacd8
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-02-11 10:50:36 +0200

    fixed eslint error

--- backend/utils/assignCategoryToWord.js
+++ backend/utils/assignCategoryToWord.js
@@ -1,20 +1,20 @@
 const technologies = require('./technologies.json')
 
 function assignCategoryToWord(word) {
-    let date = false
-    let numbers = false
+  let date = false
+  let numbers = false
 
-    const technology = technologies.find(t => t.toLowerCase() === word)
+  const technology = technologies.find(t => t.toLowerCase() === word)
 
-    if (technology) {
-        return 'Technology'
-    } else if (date) {
-        return 'date'
-    } else if (numbers) {
-        return 'numbers'
-    } else {
-        return ''
-    }
+  if (technology) {
+    return 'Technology'
+  } else if (date) {
+    return 'date'
+  } else if (numbers) {
+    return 'numbers'
+  } else {
+    return ''
+  }
 }
 
 module.exports = { assignCategoryToWord }
