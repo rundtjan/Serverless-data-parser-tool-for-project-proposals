b07dc9d4cfade0382f6e0c169dd1087214449c6d
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-02-11 11:39:36 +0200

    Words with numbers are assigned category: 'Number'

--- backend/utils/assignCategoryToWord.js
+++ backend/utils/assignCategoryToWord.js
@@ -2,16 +2,28 @@ const technologies = require('./technologies.json')
 
 function assignCategoryToWord(word) {
   let date = false
-  let numbers = false
+  let number = false
 
   const technology = technologies.find(t => t.toLowerCase() === word)
+  let letters =  word.split('')
+  
+  const isNumber = () => {
+    for (let i = 0; i < letters.length; i++) {
+      let letter = letters[i]
+      if (!isNaN(letter)) {
+        number = true 
+      }
+    }
+  }
+
+  isNumber()
 
   if (technology) {
     return 'Technology'
   } else if (date) {
     return 'date'
-  } else if (numbers) {
-    return 'numbers'
+  } else if (number) {
+    return 'Number'
   } else {
     return ''
   }
