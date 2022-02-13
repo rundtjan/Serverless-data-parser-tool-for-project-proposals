91943b2ab1556ff317dcc3bde1f11930cb523ab5
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-02-13 09:23:31 +0200

    Word including dot and number is assigned category Date

--- backend/utils/assignCategoryToWord.js
+++ backend/utils/assignCategoryToWord.js
@@ -3,26 +3,29 @@ const technologies = require('./technologies.json')
 function assignCategoryToWord(word) {
   let date = false
   let number = false
+  let dot = false
 
   const technology = technologies.find(t => t.toLowerCase() === word)
-  let letters =  word.split('')
-  
-  const isNumber = () => {
-    for (let i = 0; i < letters.length; i++) {
-      let letter = letters[i]
-      if (!isNaN(letter)) {
-        number = true 
-        return
-      }
+  let letters = word.split('')
+
+  const checkWord = () => {
+    if(letters.find(l => !isNaN(l))) {
+      number = true
     }
+    if(letters.find(l => l === '.')) {
+      dot = true
+    }
+    if(dot && number) {
+      date = true
+    } 
   }
 
-  isNumber()
+  checkWord()
 
   if (technology) {
     return 'Technology'
   } else if (date) {
-    return 'date'
+    return 'Date'
   } else if (number) {
     return 'Number'
   } else {
