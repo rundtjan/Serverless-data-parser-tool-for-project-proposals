62a4e8088c7d68b8ccae28822ca96e8d8c33677f
Jan Rundt <jan.rundt@gmail.com>
2022-02-15 09:18:31 +0200

    more front tests

--- front/src/tests/dataReducer.test.js
+++ front/src/tests/dataReducer.test.js
@@ -22,3 +22,24 @@ describe('dataReducer', () => {
     expect(newState).toBe(action.data)
   })
 })
+
+describe('dataReducer', () => {
+  test('UPDATE_MESSAGES returns correct state', () => {
+    const oldMessage = { 'message': 'testmessage' }
+    const state = [ oldMessage ]
+    const newMessage = { 'message': 'testmessage2' }
+    const newMessageArray = [ newMessage ]
+    const action = {
+      type: 'UPDATE_MESSAGES',
+      data: newMessageArray
+    }
+
+    deepFreeze(state)
+    deepFreeze(newMessage)
+    deepFreeze(action)
+    const newState = dataReducer(state, action)
+
+    expect(newState).toContain(newMessage)
+    expect(newState).toHaveLength(1)
+  })
+})
