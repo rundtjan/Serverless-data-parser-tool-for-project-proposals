271964fd2e20eb5be7420c6e2a9e1e5227853de4
Tomi Lilja <tomi.lilja@helsinki.fi>
2022-02-21 17:44:06 +0200

    Add routing and page showcasing data for slack command

--- front/src/reducers/dataReducer.js
+++ front/src/reducers/dataReducer.js
@@ -1,4 +1,5 @@
 import messageService from '../services/messages'
+import paramMessageService from '../services/paramMessages'
 
 const reducer = (state = [], action) => {
   switch(action.type) {
@@ -26,6 +27,21 @@ export const initializeMessages = (channel='general') => {
     const data = await messageService.getAll(channel)
     dispatch({
       type: 'INIT_MESSAGES',
+      data
+    })
+  }
+}
+
+/**
+ * Returns asyncronous dispatch which updates messages.
+ * @see {@link paramMessageService}
+ * @param {string} id - Id for the service
+ */
+export const getParamMessages = (id) => {
+  return async dispatch => {
+    const data = await paramMessageService.getAll(id)
+    dispatch({
+      type: 'UPDATE_MESSAGES',
       data
     })
   }
