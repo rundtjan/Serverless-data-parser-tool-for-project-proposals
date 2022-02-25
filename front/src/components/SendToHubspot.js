a86f4bba09d812299442c9ed21d92cd91de20882
Jan Rundt <jan.rundt@gmail.com>
2022-02-25 10:14:12 +0200

    pending symbol

--- front/src/components/SendToHubspot.js
+++ front/src/components/SendToHubspot.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 //Reducers
 import { sendPending, sendAssignedJSON } from '../reducers/sendReducer'
@@ -9,19 +9,21 @@ import { sendPending, sendAssignedJSON } from '../reducers/sendReducer'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress'
+import { yellow } from '@mui/material/colors'
 
 const SendToHubspot = () => {
   const dispatch = useDispatch()
+  const sendStatus = useSelector(state => state.sendStatus)
 
   const sendJson = async(event) => {
-
     event.preventDefault()
     dispatch(sendPending())
     //dispatch(clearAssignedWords())
     dispatch(sendAssignedJSON())
   }
 
-
+  //https://mui.com/components/progress/
 
   return(
     <Box
@@ -33,13 +35,31 @@ const SendToHubspot = () => {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <Grid item>
-            <Button
-              variant='outlined'
-              id='submit'
-              onClick={sendJson}
-            >
-              Send To Hubspot
-            </Button>
+            <Box sx={{ display: 'flex', alignItems: 'left' }}>
+              <Box sx={{ position: 'relative' }}>
+                <Button
+                  variant='outlined'
+                  id='submit'
+                  onClick={sendJson}
+                  disabled={sendStatus === 'pending' || sendStatus === 'error'} //this is just to test that it works
+                >
+                Send To Hubspot
+                </Button>
+                {(sendStatus === 'pending' || sendStatus === 'error') && (//'error' here just to test that it works
+                  <CircularProgress
+                    size={24}
+                    sx={{
+                      color: yellow[500],
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      marginTop: '-12px',
+                      marginLeft: '-12px',
+                    }}
+                  />
+                )}
+              </Box>
+            </Box>
           </Grid>
         </Grid>
       </Grid>
