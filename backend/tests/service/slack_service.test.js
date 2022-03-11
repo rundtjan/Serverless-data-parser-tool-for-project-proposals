d1ed8df72b2a543ee8269f3abba2c469ffe5fdec
jt551 <jtwebapps@gmail.com>
2022-03-11 11:35:16 +0200

    Add mock test for slack users. Test node-cache. Modify getChannels() to return object with names and ids.

--- backend/tests/service/slack_service.test.js
+++ backend/tests/service/slack_service.test.js
@@ -8,23 +8,59 @@ const slack = slackService({ slackClient })
 
 jest.mock('@slack/web-api', () => {
   const mock_users = jest.fn()
-  const { users_response } = require('./slack_responses')
+  const mock_channels = jest.fn()
+  const { users_response, channels_response } = require('./slack_responses')
+
   mock_users.mockReturnValue(users_response)
-  const users_Slack = {
+  mock_channels.mockReturnValue(channels_response)
+
+  const mock_Slack = {
     users: {
       list: mock_users,
     },
+    conversations: {
+      list: mock_channels,
+    }
   }
-  return { WebClient: jest.fn(() => users_Slack) }
+  return { WebClient: jest.fn(() => mock_Slack) }
 })
 
-test('return a list of posts by a user', async () => {
+test('slack returns a list of users', async () => {
   const users = await slack.getUsers()
-  expect(slackClient.users.list).toBeCalledTimes(1)
+  
   expect(users.length).toBe(3)
   expect(users).toContainEqual({
     id: 'U02U7N423SR',
     real_name: 'RealNameOne',
     username: 'testuserone'
+  })
+
+  const users_from_cache = await slack.getUsers()
+
+  expect(users_from_cache.length).toBe(3)
+  expect(users_from_cache).toContainEqual({
+    id: 'U02U7N423SR',
+    real_name: 'RealNameOne',
+    username: 'testuserone'
+  })
+  expect(slackClient.users.list).toBeCalledTimes(1)
+})
+
+test('slack returns a list of available channels', async () => {
+  const channels = await slack.getChannels()
+
+  expect(channels.length).toBe(2)
+  expect(channels).toContainEqual({
+    id: 'C02UNV80V7B',
+    name: 'test_general'
+  })
+
+  const channels_from_cache = await slack.getChannels()
+  
+  expect(slackClient.conversations.list).toBeCalledTimes(1)
+  expect(channels_from_cache.length).toBe(2)
+  expect(channels_from_cache).toContainEqual({
+    id: 'C02UNV80V7B',
+    name: 'test_general'
   })
 })
