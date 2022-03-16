6ce1e888d6344908efd6eb84a14a8a55789b9099
Tomi Lilja <tomi.lilja@helsinki.fi>
2022-03-16 14:21:08 +0200

    Add highlightReducer tests

--- front/src/tests/highlightReducer.test.js
+++ front/src/tests/highlightReducer.test.js
@@ -1,17 +1,16 @@
-/* eslint-disable no-unused-vars */
-import highlightReducer, { addHighlightedWord } from '../reducers/highlightReducer'
+import highlightReducer, { addHighlightedWord, clearHighlightedWord, clearAllHighlights } from '../reducers/highlightReducer'
 import deepFreeze from 'deep-freeze'
 import configureStore from 'redux-mock-store'
 
 const mockStore = configureStore([])
 
-describe('highlightReducer SET_HIGHLIGHTED_WORDS', () => {
+describe('highlightReducer ADD_HIGHLIGHTED_WORD', () => {
 
-  test('SET_HIGHLIGHTED_WORDS adds correct word when empty state', () => {
+  test('ADD_HIGHLIGHTED_WORD adds correct word when empty state', () => {
     const word = 'Lorem ipsum'
     const state = []
     const action = {
-      type: 'SET_HIGHLIGHTED_WORDS',
+      type: 'ADD_HIGHLIGHTED_WORD',
       word
     }
 
@@ -23,11 +22,11 @@ describe('highlightReducer SET_HIGHLIGHTED_WORDS', () => {
     expect(newState).toEqual([word])
   })
 
-  test('SET_HIGHLIGHTED_WORDS adds correct word when state has a word', () => {
+  test('ADD_HIGHLIGHTED_WORD adds correct word when state has a word', () => {
     const word = 'Lorem'
     const state = ['Ipsum']
     const action = {
-      type: 'SET_HIGHLIGHTED_WORDS',
+      type: 'ADD_HIGHLIGHTED_WORD',
       word
     }
 
@@ -51,12 +50,12 @@ describe('highlightReducer addHighlightedWord', () => {
     store.dispatch(addHighlightedWord(word))
 
     const actions = store.getActions()
-    const expectedPayload = { type: 'SET_HIGHLIGHTED_WORDS', word }
+    const expectedPayload = { type: 'ADD_HIGHLIGHTED_WORD', word }
 
     expect(actions).toEqual([expectedPayload])
   })
 
-  test('addHighlightedWord dispatches correct action', () => {
+  test('addHighlightedWord adds the word to the state', () => {
     const word = 'Customer oy'
 
     const action = store.dispatch(addHighlightedWord(word))
@@ -72,7 +71,7 @@ describe('highlightReducer addHighlightedWord', () => {
 
 describe('highlightReducer CLEAR_ALL', () => {
 
-  test('CLEAR_ALL clear all 1', () => {
+  test('CLEAR_ALL clears all 1', () => {
     const state = ['11.4.2021']
     const action = {
       type: 'CLEAR_ALL',
@@ -86,7 +85,7 @@ describe('highlightReducer CLEAR_ALL', () => {
     expect(newState).toEqual([])
   })
 
-  test('CLEAR_ALL clear all 2', () => {
+  test('CLEAR_ALL clears all 2', () => {
     const state = ['11.4.2021', 'Somethhing', 'Lorem ipsum dolor jotain']
     const action = {
       type: 'CLEAR_ALL',
@@ -98,5 +97,95 @@ describe('highlightReducer CLEAR_ALL', () => {
 
     expect(newState.length).toBe(0)
     expect(newState).toEqual([])
+  })
+})
+
+describe('highlightReducer clearAllHighlights', () => {
+  const initialState = ['11.4.2021', '100€']
+  const store = mockStore(initialState)
+
+  test('clearAllHighlights dispatches correct action', () => {
+    store.dispatch(clearAllHighlights())
+
+    const action = store.getActions()
+    const expectedPayload = { type: 'CLEAR_ALL' }
+
+    expect(action).toEqual([expectedPayload])
+  })
+
+  test('clearAllHighlights clears the state', () => {
+
+    const action = store.dispatch(clearAllHighlights())
+
+    deepFreeze(initialState)
+
+    const newState = highlightReducer(initialState, action)
+
+    expect(newState).toEqual([])
+    expect(newState.length).toBe(0)
+  })
+})
+
+describe('highlightReducer CLEAR_HIGHLIGHTED_WORD', () => {
+
+  test('CLEAR_HIGHLIGHTED_WORD clears the word', () => {
+    const word = '11.4.2021'
+    const state = ['11.4.2021']
+    const action = {
+      type: 'CLEAR_HIGHLIGHTED_WORD',
+      word
+    }
+
+    deepFreeze(state)
+
+    const newState = highlightReducer(state, action)
+
+    expect(newState.length).toBe(0)
+    expect(newState).toEqual([])
+  })
+
+  test('CLEAR_HIGHLIGHTED_WORD clears the word', () => {
+    const word = 'Lorem ipsum dolor jotain'
+    const state = ['11.4.2021', 'Somethhing', 'Lorem ipsum dolor jotain']
+    const action = {
+      type: 'CLEAR_HIGHLIGHTED_WORD',
+      word
+    }
+
+    deepFreeze(state)
+
+    const newState = highlightReducer(state, action)
+
+    expect(newState.length).toBe(2)
+    expect(newState).toEqual(['11.4.2021', 'Somethhing'])
+  })
+})
+
+describe('highlightReducer clearHighlightedWord', () => {
+  const initialState = ['11.4.2021', '100€']
+  const store = mockStore(initialState)
+
+  test('clearHighlightedWord dispatches correct action', () => {
+    const word = '11.4.2021'
+
+    store.dispatch(clearHighlightedWord(word))
+
+    const actions = store.getActions()
+    const expectedPayload = { type: 'CLEAR_HIGHLIGHTED_WORD', word }
+
+    expect(actions).toEqual([expectedPayload])
+  })
+
+  test('clearHighlightedWord dispatches correct action', () => {
+    const word = '100€'
+
+    const action = store.dispatch(clearHighlightedWord(word))
+
+    deepFreeze(initialState)
+
+    const newState = highlightReducer(initialState, action)
+
+    expect(newState).toEqual(['11.4.2021'])
+    expect(newState.length).toBe(1)
   })
 })
