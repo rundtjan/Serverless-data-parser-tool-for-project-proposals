09c0ad9007014e19cc315b5adb9efe9f37d1104a
Jan Rundt <jan.rundt@gmail.com>
2022-03-24 15:14:42 +0200

    better parsing

--- api/utils/parseShortcutPayload.js
+++ api/utils/parseShortcutPayload.js
@@ -1,19 +1,14 @@
-const parseReqBody = require('./parseReqBody')
-
 module.exports = (event) => {
-    const thread_ts = event.body.split('thread_ts%22%3A%22')[1].split('%')[0]
-    event = parseReqBody(event)
-    const payloadArr = event.body.payload.split('%22%2C%22')
-    payload = {}
-    payloadArr.forEach(element => {
-        arr = element.split('%22%3A%22')
-        if (arr.length > 1) payload[arr[0]] = decodeURI(arr[1])
-    });
-    const channel = payload.name.split('"')[0]
+    let data = event.body
+    let buff = Buffer.from(data, 'base64');
+    event.body = buff.toString();
+    let decoded = decodeURIComponent(event.body)
+    let json = JSON.parse(decoded.split('=')[1])
+    const channelId = json.channel.id 
+    const thread_ts = json.message.thread_ts
+    const response_url = json.response_url
     let text = ''
-    payload.text.split('+').forEach(element => text += `${element} `)
+    json.message.text.split('+').forEach(element => text += `${element} `)
     text = text.substring(0, 50) + '...'
-    urlArr = payload.response_url.split('\\%2F')
-    const response_url = `https://hooks.slack.com/app/${urlArr[4]}/${urlArr[5]}/${urlArr[6]}`    
-    return { thread_ts, channel, text, response_url }
+    return { thread_ts, channelId, text, response_url }
 }
