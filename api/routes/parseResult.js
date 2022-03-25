3342f7a76d6c1f2c488ba2f83a31a110a0da6029
Jan Rundt <jan.rundt@gmail.com>
2022-03-25 15:50:49 +0200

    message shortcuts

--- api/routes/parseResult.js
+++ api/routes/parseResult.js
@@ -4,21 +4,29 @@ const { parseTimestamp } = require('../utils/parseSlackTimestamp')
 
 module.exports = async function (event) {
     console.log('Receiving post from Parsa front for parsing.');
-
     let data = event.body
-    let buff = new Buffer(data, 'base64');
+    let buff = Buffer.from(data, 'base64');
     event.body = buff.toString('ascii');
     event = parseReqBody(event)
     console.log('eventbody', event.body)
-
-    const args = {channel: event.body.channel, user: event.body.user, oldest: event.body.oldest}
-    if (!args.oldest) args.oldest = parseTimestamp(Date.now() * 1000, event.body.hours)
-    console.log('args', args)
-
-    try {
-        const response = await slackController.slackMessages(args)
-        return response
-    } catch (error) {
-        return error.error
+    if (event.body.type && event.body.type == 'thread'){
+        try {
+            const response = await slackController.getAllMessagesFromSingleThread(event.body)
+            console.log('respose in get thread', response)
+            return response
+        } catch (error) {
+            console.log('error in get thread', error)
+            return error.error
+        }
+    } else {
+        const args = {channel: event.body.channel, user: event.body.user, oldest: event.body.oldest}
+        if (!args.oldest) args.oldest = parseTimestamp(Date.now() * 1000, event.body.hours)
+    
+        try {
+            const response = await slackController.slackMessages(args)
+            return response
+        } catch (error) {
+            return error.error
+        }
     }
 }
