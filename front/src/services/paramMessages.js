49b607aa2f79d66b50e24ca3c4fcd86e0074cfe4
Jan Rundt <jan.rundt@gmail.com>
2022-03-25 11:19:41 +0200

    eslint disable process.env

--- front/src/services/paramMessages.js
+++ front/src/services/paramMessages.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = `${process.env.REACT_APP_API_URL}?route=parseResult`
+const baseUrl = `${process.env.REACT_APP_API_URL}?route=parseResult` // eslint-disable-line no-eval
 
 /**
  * Gets data from backend that matches the parameters given in slack with the parse command.
