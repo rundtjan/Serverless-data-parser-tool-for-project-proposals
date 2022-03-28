f2316ab2f77fa0b8404ef0da830163d53ed6c750
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2022-03-28 14:45:56 +0300

    trying to fix buffer error

--- api/routes/searchHubspot.js
+++ api/routes/searchHubspot.js
@@ -1,10 +1,11 @@
 const hubspotController = require('../controllers/hubspotController')
 
 module.exports = async function (event) {
+    console.log(event)
     console.log(event.body)
     let data = event.body
-    let buff = buffer.from(data, 'base64')
-    const reqObj = JSON.parse(buff.toString('ascii'))
+    let buff = Buffer.from(data.toString('ascii'), 'base64')
+    const reqObj = JSON.parse(buff)
     console.log(reqObj)
 
     try {
