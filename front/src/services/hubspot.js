0d84bd43976e2c464fcc58fb16f904f153b260d4
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-03-31 16:25:12 +0300

    Test service for deal update

--- front/src/services/hubspot.js
+++ front/src/services/hubspot.js
@@ -1,12 +1,27 @@
 import axios from 'axios'
 
-const baseUrl = `${process.env.REACT_APP_API_URL}?route=searchDeals` // eslint-disable-line
+const baseUrl = `${process.env.REACT_APP_API_URL}` // eslint-disable-line
 
 const searchDeals = async(queryString) => {
+  const searchUrl = `${baseUrl}?route=searchDeals`
   const sendData = { queryString: queryString }
-  const res = await axios.post(baseUrl, JSON.stringify(sendData))
+  const res = await axios.post(searchUrl, JSON.stringify(sendData))
 
   return res.data.results
 }
+const updateDeal = async(/*properties, id*/) => {
+  console.log('in updateDeal')
+  const property = { amount: 5, dealname: 'Deal kauppisen muutettu maansiirtofirma oy ' }
+  const id = 8319711901
+  const sendData = { dealId: id, properties: property }
+  console.log('sendData ' + JSON.stringify(sendData))
+  const updateUrl = `${baseUrl}?route=updateDeal`
+  const res = await axios.post(updateUrl, JSON.stringify(sendData))
+  console.log('res ' + JSON.stringify(res))
+  return res.data
+}
 
-export default { searchDeals }
+updateDeal()
+
+export default { searchDeals, updateDeal }
+
