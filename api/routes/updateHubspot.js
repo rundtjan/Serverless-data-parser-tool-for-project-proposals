f0abe1a0ae47d5c4366e90c56516630de63ff4c2
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-04-03 10:11:47 +0300

    More documentation

--- api/routes/updateHubspot.js
+++ api/routes/updateHubspot.js
@@ -1,16 +1,19 @@
 const hubspotController = require('../controllers/hubspotController')
-
+/**
+ * A function that takes care of requests of type 'POST route=updateDeals' containing the
+ * parameters to use as simplePublicObjectInput and dealId.
+ * @param {*} event an object passed as parameters to the lambda, contains
+ * dealId and properties to be updated.
+ * @returns the response from Hubspot or an error.
+ */
 module.exports = async function (event) {
     console.log('in api routes updateHubspot')
     let data = event.body
     let buff = Buffer.from(data, 'base64')
     const sendJson = JSON.parse(buff.toString('utf8'))
-    //console.log('sendJson id ' + JSON.stringify(sendJson.dealId))
-    //console.log('sendJson properties ' + JSON.stringify(sendJson.properties))
     try {
         const result = await hubspotController.updateDeal(sendJson.properties, sendJson.dealId)
-        //console.log('in api routes updateDeal result ' + JSON.stringify(result))
-        if (result) return result
+        if (result.id) return result
         return 'error'
     } catch (error) {
         console.log(error)
