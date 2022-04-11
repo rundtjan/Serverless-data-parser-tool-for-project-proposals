845e3d5b3d2b2d5342a02fedf09afd548872b295
Jan Rundt <jan.rundt@gmail.com>
2022-04-11 13:41:23 +0300

    clearer message

--- api/utils/parseSlackNotification.js
+++ api/utils/parseSlackNotification.js
@@ -15,7 +15,7 @@ const response = (customer, action, type, link, dealname) => {
       'type': 'section',
       'text': {
         'type': 'mrkdwn',
-        'text': `A deal with customer ${customer} has been ${action} in Hubspot using parsed data from this ${type}.`
+        'text': `A deal with customer ${customer} has been ${action} in Hubspot based on a parse-action in this ${type}.`
       }
     },
     {
