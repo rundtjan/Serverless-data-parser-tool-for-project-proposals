3cc3c607ea34bf2c8ffe7724e18b995379a8eacd
Eevi Bengs <eevi.bengs@helsinki.fi>
2022-04-12 08:04:43 +0300

    Company category

--- api/utils/assignCategoryToWord.js
+++ api/utils/assignCategoryToWord.js
@@ -1,15 +1,16 @@
 const { technologies } = require('./technologies')
 
 /**
- * 
+ * Checks if a given word belongs to any category. 
  * @param {word from splitted slack messages} word 
- * @returns A string with the category which the category name the word belongs to or '' if the word has no category.
+ * @returns A string with the category whith the category name the word belongs to or '' if the word has no category.
  */
 function assignCategoryToWord(word) {
   let date = false
   let number = false
   let specialCharacter = false
   let specials = false
+  let company = false
 
   // Goes through list with technology words.
   const technology = technologies.has(word)
@@ -17,6 +18,9 @@ function assignCategoryToWord(word) {
   let letters = word.split('')
 
   const checkWord = () => {
+    if(letters.find(l => l=== ' ')) {
+      company = true
+    }
     if(letters.find(l => l === '€' || l === '$')) {
       specials = true
     }
@@ -37,6 +41,8 @@ function assignCategoryToWord(word) {
   
   if (technology) {
     return 'Technology'
+  } else if (company) {
+    return 'Company' 
   } else if (date) {
     return 'Date'
   } else if (number) {
